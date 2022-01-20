545af86c4a30459e74996056ab7ce98c515fcdce
Code-w-e-e <87078854+Code-w-e-e@users.noreply.github.com>
2022-01-20 22:45:01 +0300

    Weather App
    Создание weather app по туториалу от Jonah Lawrence • Dev Pro Tips(youtube)
    Учебный проект, который знакомит с api

---
+++ app.js
@@ -0,0 +1,62 @@
+
+let weather ={
+
+    apiKey: "ce3c6dc74be2caa727b98953df17d1df",
+    fetchWeather: function(city){
+
+        
+        
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' 
+        + city 
+        +'&units=metric&appid=' 
+        + this.apiKey
+        )
+        .then(function(resp){return resp.json()})
+        .then((data)=>this.displayWeather(data))
+        .catch(function(){
+        });
+
+
+    },
+
+    displayWeather: function(data){
+        const {name}=data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+       //console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+icon+".png";
+        document.querySelector(".description").innerText =  description;
+        if(temp > 0){
+            document.querySelector(".temp").innerText =  "+" + Math.round(temp) + "°C";
+        } else{
+            document.querySelector(".temp").innerText =  Math.round(temp) + "°C";
+        }
+        document.querySelector(".humidity").innerText =  "Humidity: " + humidity +"%";
+        document.querySelector(".wind").innerText =  "Wind speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        
+    },
+
+    search: function(){
+        this.fetchWeather(document.querySelector(".search-field").value);
+
+    },
+};
+
+
+document.querySelector(".search button").addEventListener("click", function(){
+    weather.search();
+    document.querySelector(".search-field").value="";
+}); 
+
+ document.querySelector(".search-field").addEventListener("keyup", function(event){
+    if(event.key == "Enter"){
+        weather.search();
+        document.querySelector(".search-field").value="";
+    }
+}); 
+
+
+weather.fetchWeather("Moscow");
